f50f2c28521ab7a78e56b68ef09fbeb68bc8d277
Noel-96 <noelorion69@gmail.com>
2019-12-03 10:56:36 +0100

    Initial Commit

---
+++ GOGOVAN-MOBILE-DEMO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GOGOVAN-MOBILE-DEMO
+//
+//  Created by Noel Obaseki on 03/12/2019.
+//  Copyright © 2019 NoelObaseki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
